4e2334702c0ffb58246a7a6b02dcf3e5310fb309
PULLIGANI ROOPESH <roopeshp771@gmail.com>
2026-01-31 14:17:46 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,11 @@
+public class Demo2
+{
+    public static void main(String[] args)
+    {
+        int a=23;
+        int b=27;
+        int c;
+        c=a+b;
+        System.out.println("the sum:"+c);
+    }
+}
