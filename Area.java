e03e56d667d8f2cd8c69049e5a7655a3117447ca
PULLIGANI ROOPESH <roopeshp771@gmail.com>
2026-01-31 14:27:45 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,10 @@
+public class Area
+{
+    public static void main(String[] args)
+    {
+        int r=5;
+        double z;
+        z=3.14*5*5;
+        System.out.println("the area of the circle:"+z);
+    }
+}
